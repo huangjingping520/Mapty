361be9bc472c943e6d424beb0bc07583f7ccc58a
MerlinAlex(HJP) <merlinalexhjp@gmail.com>
2022-05-06 23:31:34 +0800

    feat: 显示地图

--- script.js
+++ script.js
@@ -14,9 +14,21 @@ if (navigator.geolocation) {
     const { latitude } = position.coords
     const { longitude } = position.coords
     // Google Maps
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+    // console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
     // Baidu Maps
     // console.log(`https://map.baidu.com/@${latitude},${longitude}`)
+
+    const coords = [latitude, longitude]
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(coords).addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
   }, function () {
     alert('Could not get your position!!!')
   })
