aa33677bbdc0d5f8102bbd64d5d06651699ad4ba
MerlinAlex(HJP) <merlinalexhjp@gmail.com>
2022-05-06 22:59:53 +0800

    feat: 获取位置

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-'use strict';
-
-// prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
@@ -10,3 +7,14 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// 获取当前位置
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const { latitude } = position.coords
+    const { longitude } = position.coords
+    console.log(latitude, longitude)
+  }, function () {
+    alert('Could not get your position!!!')
+  })
+}
