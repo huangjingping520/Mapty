772d8ec8c3fae3cdedf421e4525a931c241ac8c1
MerlinAlex(HJP) <merlinalexhjp@gmail.com>
2022-05-07 17:13:39 +0800

    feat: form

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ const inputDuration = document.querySelector('.form__input--duration')
 const inputCadence = document.querySelector('.form__input--cadence')
 const inputElevation = document.querySelector('.form__input--elevation')
 
+let map, mapEvent
+
 // 获取当前位置
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -34,28 +36,17 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude]
 
-      const map = L.map('map').setView(coords, 13)
+      map = L.map('map').setView(coords, 13)
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map)
 
-      map.on('click', function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng
-
-        const coords = [lat, lng]
-        L.marker(coords)
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 200,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false
-            })
-          )
-          .openPopup()
+      map.on('click', function (mapE) {
+        mapEvent = mapE
+        form.classList.remove('hidden')
+        inputDistance.focus()
       })
     },
     function () {
@@ -63,3 +54,21 @@ if (navigator.geolocation) {
     }
   )
 }
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  const { lat, lng } = mapEvent.latlng
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 200,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup'
+      })
+    )
+    .setPopupContent(`<div>${lat}, ${lng}</div>`)
+    .openPopup()
+})
