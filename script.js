4716f746f0713bb291f73b502fb71bbda78660c5
MerlinAlex(HJP) <merlinalexhjp@gmail.com>
2022-05-07 15:53:53 +0800

    chore: prettier

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector('.form')
+const containerWorkouts = document.querySelector('.workouts')
+const inputType = document.querySelector('.form__input--type')
+const inputDistance = document.querySelector('.form__input--distance')
+const inputDuration = document.querySelector('.form__input--duration')
+const inputCadence = document.querySelector('.form__input--cadence')
+const inputElevation = document.querySelector('.form__input--elevation')
 
 // 获取当前位置
 if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function (position) {
-    const { latitude } = position.coords
-    const { longitude } = position.coords
-    // Google Maps
-    // console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
-    // Baidu Maps
-    // console.log(`https://map.baidu.com/@${latitude},${longitude}`)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords
+      const { longitude } = position.coords
+      // Google Maps
+      // console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+      // Baidu Maps
+      // console.log(`https://map.baidu.com/@${latitude},${longitude}`)
 
-    const coords = [latitude, longitude]
+      const coords = [latitude, longitude]
 
-    const map = L.map('map').setView(coords, 13);
+      const map = L.map('map').setView(coords, 13)
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map)
 
-    L.marker(coords).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
-  }, function () {
-    alert('Could not get your position!!!')
-  })
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup()
+    },
+    function () {
+      alert('Could not get your position!!!')
+    }
+  )
 }
